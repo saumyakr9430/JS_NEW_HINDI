dad94a88d7b6d1769447b35ffd2c168c500602ec
saumyakr9430 <saumyakr9430@gmail.com>
2025-02-20 21:45:36 +0530

    ne32

--- 01basics/test.js
+++ 01basics/test.js
@@ -1,5 +1,10 @@
-console.log("saumya")
-let number = 0
-let boolnum = Boolean(number)
-console.log(boolnum)
-console.log(typeof boolnum)
+// console.log("saumya")
+// let number = 0
+// let boolnum = Boolean(number)
+// console.log(boolnum)
+// console.log(typeof boolnum)
+
+let x = 3;
+const y = x++;
+console.log(x) 
+console.log(y) 
